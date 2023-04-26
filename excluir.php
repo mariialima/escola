c779d491017d9cb6664480d2e7e17014d22032c5
mariialima <mariiaeduardafl@gmail.com>
2023-04-26 09:37:42 -0300

    o

--- excluir.php
+++ excluir.php
@@ -1,6 +1,6 @@
 <?php
 include("conecta.php");
-$matricula = $_POST["matricula"];
+$matricula = $_GET["M"];
 
 $comando = $pdo->prepare("DELETE FROM alunos WHERE matricula = $matricula");
 $resultado = $comando->execute();
